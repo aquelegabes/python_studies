d21a9d4cb5b63f3e017625d9df477d90ce8bccb4
Gabriel Santos <gabriel.santana@gpnet.com.br>
2019-04-15 15:56:11 -0300

    definetly going somewhere (i think LUL)

---
+++ BlackHat-Python-BookExercises/Creativity_Testing/ipobjects.py
@@ -0,0 +1,57 @@
+import socket
+import struct
+from ctypes import Structure, c_ubyte, c_ushort, c_ulong
+
+class IP(Structure): #(1)
+    _fields_ = [
+        ("ihl",             c_ubyte, 4),
+        ("version",         c_ubyte, 4),
+        ("tos",             c_ubyte),
+        ("len",             c_ushort),
+        ("id",              c_ushort),
+        ("offset",          c_ushort),
+        ("ttl",             c_ubyte),
+        ("protocol_num",    c_ubyte),
+        ("sum",             c_ushort),
+        ("src",             c_ulong),
+        ("dst",             c_ulong)
+    ]
+
+    def __new__(self, socket_buffer=None):
+        return self.from_buffer_copy(socket_buffer)
+    
+    def __init__(self, socket_buffet=None):
+        # mapeia constantes do protocolo aos seus nomes
+        self.protocol_map = {1:"ICMP",6:"TCP", 17:"UDP"}
+
+        # endereços IP legíveis #(2)
+        self.src_address = socket.inet_ntoa(struct.pack("<L",self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L",self.dst))
+
+        # protocolo legível
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
+
+class ICMP(Structure): #(1)
+	_fields_ = [
+		("type",        c_ubyte),
+		("code",        c_ubyte),
+		("checksum",    c_ushort),
+		("unused",		c_ushort),
+		("next_hop_mtu",c_ushort)
+	]
+
+	def __new__(self, socket_buffer):
+		return self.from_buffer_copy(socket_buffer)
+	
+	def __init__(self, socket_buffer):
+		pass
+
+def SocketTCP():
+    return socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+def SocketUDP():
+    return socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
---
+++ BlackHat-Python-BookExercises/Creativity_Testing/scannet.py
@@ -0,0 +1,99 @@
+''' #!/usr/bin/python '''
+# -*- coding: utf-8 -*-
+
+import ipobjects
+import sys
+import os
+import socket
+import getopt
+from subprocess import call
+
+# globals
+listen = False
+connect = False
+sender = ""
+target = ""
+typeconn = ""
+port = int()
+
+def help():
+    print ("ScanNet -- Scan, listen, connect")
+    print ("-s --scan \tEscaneia a rede para entradas de hosts")
+    print ("-t --target \tDefine um alvo. ex: -t 192.168.1.21")
+    print ("-p --port \tDefine uma porta. ex: -p 53125")
+    print ("-l --listen \tEscutar na conexão padrão TCP")
+    print ("-c --connect \tConecta e envia algo na conexão padrão TCP. ex: -c 123feijaoarroz")
+    print ("-udp \tDefine porta UDP para conexão")
+    print ("-tcp \tDefine porta TCP para conexão")
+    sys.exit(0)
+
+def scanning():
+    ipv4s = []
+    subnets = []
+    i = 0
+
+    if os.name == "nt": #windows
+        call('ipconfig | findstr -i ipv4 > ipv4.txt', shell=True)
+        call('ipconfig | findstr -i sub > subnet.txt', shell=True)
+
+    # we'll assume it's only going to be 2 lines
+    file = open("ipv4", "r")
+
+    for line in file:
+        ipv4s.append(line),
+
+    localhost = ipv4s[1].split(' ')
+
+def listening(target,port,typeconn):
+    print()
+
+def connecting(target,port,typeconn,sender):
+    print()
+
+def main():
+    global listen
+    global port
+    global target
+    global connect
+    global sender
+    global typeconn
+
+    if not len(sys.argv[1:]):
+        help()
+    
+    try:
+        opts, args = getopt.getopt(
+                        sys.argv[1:],
+                        "hle:t:p:cu",
+                        ["help", "listen", "target", "port", "connect", "scan", "udp", "tcp"])
+    except getopt.GetoptError as err:
+        print (str(err))
+        help()
+
+    for options,argument in opts:
+        if options in ("-h", "--help"):
+            help()
+        elif options in ("-s", "--scan"):
+            scanning()
+        elif options in ("-l", "--listen"):
+            listen = True
+        elif options in ("-c","--connect"):
+            connect = True
+            sender = argument
+        elif options in ("-t", "--target"):
+            target = argument
+        elif options in ("-p", "--port"):
+            port = int(argument)
+        elif options in ("-udp", "-tcp"):
+            typeconn = argument
+        else:
+            assert False,"Opção inválida"
+
+    if listen and len(target) and port > 0:
+        if not len(typeconn):
+            typeconn = "TCP"
+        
+        if len(sender):
+            connecting(target,port,typeconn,sender)
+        else:
+            listening(target,port,typeconn)
