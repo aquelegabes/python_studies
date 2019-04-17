f5ee65bc13331004a5b575501fc9c86c9438149a
Gabriel Santos <gabriel.santana@gpnet.com.br>
2019-04-17 16:10:24 -0300

    time converter done

--- HackerRank-SOLUTIONS/Time_Conversion/solution.py
+++ HackerRank-SOLUTIONS/Time_Conversion/solution.py
@@ -6,17 +6,53 @@ import re
 # Complete the timeConversion function below.
 #
 def CheckForCorrectStringStamp(text):
-    textLength = len(text)
-    lastChars = text[-2:]
-    pattern = r"'am|pm'gmi"
-    match = re.search(pattern,text)
-    print (match)
-    
+    pattern = "am|pm"
+    match = re.search(pattern,text,re.I)    
+    return match.group(0)    
+
+def correctHours(timestamp, time):
+    match = re.search("pm",timestamp,re.I)
+    after2digits = time[2:]
+    if (match and match.group(0)):
+        if int(time[:2]) == 12:
+            return "12%s" % (after2digits)
+        elif int(time[:2]) == 1:
+            return "13%s" % (after2digits)
+        elif int(time[:2]) == 2:
+            return "14%s" % (after2digits)
+        elif int(time[:2]) == 3:
+            return "15%s" % (after2digits)
+        elif int(time[:2]) == 4:
+            return "16%s" % (after2digits)
+        elif int(time[:2]) == 5:
+            return "17%s" % (after2digits)
+        elif int(time[:2]) == 6:
+            return "18%s" % (after2digits)
+        elif int(time[:2]) == 7:
+            return "19%s" % (after2digits)
+        elif int(time[:2]) == 8:
+            return "20%s" % (after2digits)
+        elif int(time[:2]) == 9:
+            return "21%s" % (after2digits)
+        elif int(time[:2]) == 10:
+            return "22%s" % (after2digits)
+        elif int(time[:2]) == 11:
+            return "23%s" % (after2digits)
+    else:
+        if int(time[:2]) == 12:
+            return "00%s" % (after2digits)
+        return time
 
 def timeConversion(s):
     if(CheckForCorrectStringStamp(s)):
-        return "lol"
+        # obtaining only the format (PM/AM)
+        timestamp = s[-2:]
+        # removing PM/AM from string
+        s = s[:-2]
+        return correctHours(timestamp,s)
+    else:
+        return (s[:-2])
 
-s = "08:48:31PM"
+s = "12:48:31AM"
 result = timeConversion(s)
 print (result)
