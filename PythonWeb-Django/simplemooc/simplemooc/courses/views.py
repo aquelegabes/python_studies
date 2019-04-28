a9e52072432c2b73cce7aba1fa8d41bfd2b81104
Gabriel Santos <galexander.santos1@gmail.com>
2019-04-28 20:45:53 -0300

    forms in python, pretty interesting

--- PythonWeb-Django/simplemooc/simplemooc/courses/views.py
+++ PythonWeb-Django/simplemooc/simplemooc/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 # Create your views here.
 def index(request):
@@ -12,8 +13,20 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            valid = context['is_valid'] = True
+            print(form.cleaned_data['name'])
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+
     context = {
-        'course': course
+        'course': course,
+        'form': form,
+        'is_valid': valid
     }
     template_name = 'courses/details.html'
     return render(request, template_name, context)
