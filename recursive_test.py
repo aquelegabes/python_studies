d94780b345cff6e3b7ba2fab7d1e03deb6e973f9
Gabriel Santos <34673909+gabesantos1@users.noreply.github.com>
2019-05-30 13:16:10 -0300

    fixing errors

--- recursive_test.py
+++ recursive_test.py
@@ -1,10 +1,10 @@
 def Factorial(number):
-	if (number == 1):
+	if (number == 1 or number == 0):
 		return 1
 	else:
 		return number * Factorial(number -1)
 
 def OneLinerFactorial(number):
-	return 1 if number == 1 else number * Factorial(number - 1)
+	return 1 if num == 1 or num == 0 else num * Factorial(num - 1)
 	
 print(Factorial(5))
